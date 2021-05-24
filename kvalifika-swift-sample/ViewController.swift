5228bf7e8d8aeaeb08a680938ac1739e94988bb1
Levan Gurbeleishvili <gurbela@gmail.com>
2021-05-24 19:04:42 +0400

    Initial Commit

---
+++ kvalifika-swift-sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kvalifika-swift-sample
+//
+//  Created by Levan Gurbeleishvili on 5/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
