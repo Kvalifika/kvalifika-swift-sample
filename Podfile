c8b7c066aa5564c21f48b7d5525b2ca90995671e
Levan Gurbeleishvili <gurbela@gmail.com>
2021-05-24 19:18:06 +0400

    init sample

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'kvalifika-swift-sample' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for kvalifika-swift-sample
+  pod 'Kvalifika', :git => "https://github.com/Kvalifika/kvalifika-swift-sdk.git"
+
+end
--- kvalifika-swift-sample/ViewController.swift
+++ kvalifika-swift-sample/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import Kvalifika
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Initialize SDK
+        Kvalifika.initializeSDK(
+                AppID: "26d3f2ee-4f37-4376-8357-c4114b9d2c63", 
+                AppLogoImageName: "AppLogo", 
+                DocLogoImageName: "DocLogo", 
+                CancelImageName: "cancel", 
+                LanguageLocale:  Kvalifika.LanguageLocaleTypeEnum.GE.rawValue, 
+                onInitialize: onInitialize, 
+                onStart: onStart, 
+                onFinish: onFinish, 
+                onError: onError
+            )
+        
     }
-
-
+    
+    
+    func onInitialize() {
+        print("onInitialize")
+    }
+    
+    func onStart(sessionId: String) {
+        print(sessionId, "started")
+    }
+    
+    func onFinish(sessionId: String) {
+        print(sessionId, "onFinish")
+    }
+    
+    func onError(error: Kvalifika.KvalifikaSDKErrorEnum) {
+        print(error)
+    }
+    
+    @IBAction func onStartButtonAction(_ sender: Any) {
+        let _ = Kvalifika.startPhotoIDScan(onViewController: self)
+    }
+    
 }
 
